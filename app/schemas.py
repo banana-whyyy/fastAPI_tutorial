81e0d30920f0b5a8365af7281759c4f28f3a7b1a
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-05-04 12:40:03 +0300

    Создал schemas для книг, авторов и пользователей

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,10 @@
 # Описание того, как выглядит JSON, который приходит и уходит через API
 from pydantic import BaseModel, Field, ConfigDict
+import uuid
+from fastapi_users import schemas
 
 
-
+# Описание схем автороов
 class AuthorBase(BaseModel):
     name: str = Field(..., min_length=3, max_length=100, description="Author Name")
 
@@ -17,5 +19,38 @@ class AuthorRead(AuthorBase):
 
 
 class AuthorUpdate(AuthorBase):
-    name: str | None = None
+    name: str | None = Field(None, min_length=3, max_length=100, description="Author Name")
+    model_config = ConfigDict(from_attributes=True)
 
+
+# Описание схем книг
+class BookBase(BaseModel):
+    title: str = Field(..., min_length=1, max_length=200, description="Book title")
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class BookRead(BookBase):
+    id: int = Field(..., description="Unique book ID")
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BookUpdate(BookBase):
+    title: str | None = Field(None, min_length=1, max_length=200, description="Book title")
+    author_id: int | None = None
+    model_config = ConfigDict(from_attributes=True)
+
+
+# Описание схем пользователей
+class UserCreate(schemas.BaseUserCreate):
+    nickname: str
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    nickname: str
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    nickname: str
